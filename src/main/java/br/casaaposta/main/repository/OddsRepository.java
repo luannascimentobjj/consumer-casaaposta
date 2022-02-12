c8cabb2c704068e17569a7d0bb0e013985ec904d
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-02-12 14:02:08 -0300

    migração para o java 18
    Inclusão dos arquivos repositório.
    migração para o java 18

---
+++ src/main/java/br/casaaposta/main/repository/OddsRepository.java
@@ -0,0 +1,12 @@
+package br.casaaposta.main.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import br.casaaposta.main.entity.Odds;
+
+public interface OddsRepository extends CrudRepository<Odds, Long> {
+
+	
+		
+	}
+	
+
---
+++ src/main/java/br/casaaposta/main/repository/ResultadoRepository.java
@@ -0,0 +1,12 @@
+package br.casaaposta.main.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import br.casaaposta.main.entity.Resultado;
+
+public interface ResultadoRepository extends CrudRepository<Resultado, Long>{
+	
+
+		
+	}
+
+
