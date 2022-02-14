39a4f48f2f1d8d2d48298253e44f5abd048d1c20
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-02-13 23:51:21 -0300

    Update FutServiceBusiness
    Acessando as células internas para montar o objeto.
    Faltando apenas instanciar o objeto novo e criar suas subclasses.
    
    Necessário replicar a estrutura para o Odds.

--- src/main/java/br/casaaposta/main/business/FutServiceBusiness.java
+++ src/main/java/br/casaaposta/main/business/FutServiceBusiness.java
@@ -1,9 +1,7 @@
 package br.casaaposta.main.business;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import org.springframework.stereotype.Component;
 import br.casaaposta.main.entity.Resultado;
 import lombok.Data;
@@ -12,36 +10,55 @@ import lombok.Data;
 @Component
 public class FutServiceBusiness {
 
-	
-	public Resultado bindResultado (LinkedHashMap<Object, Object> objects) {
-		
-		//Object obj = objects;
-		
+	public Resultado bindResultado(LinkedHashMap<Object, Object> objects) {
+
+		// Object obj = objects;
+
 		objects.entrySet().forEach(entry -> {
 			entry.getKey();
 			entry.getValue();
-			if(entry.getKey().equals("Lines")) {
-				Map<Object, Object> lines = (Map<Object, Object>) entry.getValue();
-				lines.entrySet().forEach(line -> {
-					Class<? extends Object>  liness = line.getClass();
+			if (entry.getKey().equals("Lines")) {
+				ArrayList<LinkedHashMap<Object, Object>> lines = (ArrayList<LinkedHashMap<Object, Object>>) entry.getValue();
+			  	lines.forEach(line -> {
+					line.entrySet().forEach(subline -> {
+						
+						if(subline.getKey().equals("Hora")) {
+							//Seta a variavel hora - integer
+						}
+						
+						if(subline.getKey().equals("Percents")) {
+							//Seta a variavel Percents - double
+						}
+						
+						if (subline.getKey().equals("Cells")) {
+							ArrayList<LinkedHashMap<Object, Object>> results = (ArrayList<LinkedHashMap<Object, Object>>) subline.getValue();
+							results.forEach(result -> {
+								result.entrySet().forEach(res ->{
+									if(res.getKey().equals("Minute")) {
+										//seta o resultado - integer
+									}else if(res.getKey().equals("Tooltip")) {
+										//seta o tooltip - string
+									}else if(res.getKey().equals("Result")) {
+										//seta o result - string
+									}else if(res.getKey().equals("Countable")) {
+										//seta o contable - boolean
+									}else if(res.getKey().equals("SumScore")) {
+										//seta o sumscore - integer
+									}
+									
+								});
+							});
+						}
+					});
+
 				});
-				
-			
-				
+
 			}
-		    
+
 		});
-		
-		//for (Map.Entry<Object, ArrayList<Object>> entry : objects.entrySet()) {
-			//Object key = entry.getKey();
-			//Object value = entry.getValue();
-			
-			
-			
-		///}
-		
+
 		return null;
-		
+
 	}
-	
+
 }
