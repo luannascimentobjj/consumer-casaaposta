0b56aa19b8dadb8a38e117a8ec5518df8f5e6475
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-02-21 22:11:56 -0300

    Alteração do nome do projeto para consumer-casaaposta

---
+++ src/main/java/br/casaaposta/main/ConsumerCasaApostaApplication.java
@@ -0,0 +1,33 @@
+package br.casaaposta.main;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@Configuration
+public class ConsumerCasaApostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerCasaApostaApplication.class, args);
+	}
+
+	@Bean  
+	public Executor asyncExecutor() {
+        final ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(1);
+        taskExecutor.setMaxPoolSize(4);
+        taskExecutor.setThreadNamePrefix("taskExecutor");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
